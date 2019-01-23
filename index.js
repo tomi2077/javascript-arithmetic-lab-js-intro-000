e156b563742998b96af30f8d40c789c218a29474
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2019-01-23 12:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,20 +23,9 @@ function dec(n){
 }
 
 function makeInt(n){
-  return parseInt(n)
+  var n  = pasrseInt(n)
+  return n
 }
-
-function roughScale(x, base) {
-  var parsed = parseInt(x, base);
-  if (isNaN(parsed)) { return 0 }
-  return parsed * 100;
-}
-
-console.log(roughScale(' 0xF', 16));
-// expected output: 1500
-
-console.log(roughScale('321', 2));
-// expected output: 0
 
 
 function makeInt(n){
